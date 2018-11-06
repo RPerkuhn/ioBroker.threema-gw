78d6587e518b4a834cfbce84bc6d167b9348e1a8
Ralf Perkuhn <44618116+RPerkuhn@users.noreply.github.com>
2018-11-06 19:20:28 +0100

    Code cleanup and comments

--- threema-gw.js
+++ threema-gw.js
@@ -1,12 +1,6 @@
 'use strict';
 const utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
-
 const adapter = new utils.Adapter('threema-gw');
-
-/*Variable declaration, since ES6 there are let to declare variables. Let has a more clearer definition where 
-it is available then var.The variable is available inside a block and it's childs, but not outside. 
-You can define the same variable name inside a child without produce a conflict with the variable of the parent block.*/
-let variable = 1234;
 
 // is called when adapter shuts down - callback has to be called under any circumstances!
 adapter.on('unload', function (callback) {
@@ -14,35 +8,18 @@ adapter.on('unload', function (callback) {
         adapter.setState('info.connection', {val: false}); //connection to Threema gateway not established
         adapter.setState('info.credits', {val: -999});    //set credits at Threema gateway to a defaul value (-999 = 'unknown')
         adapter.setState('info.lastresponse', {val: 'adapter is not running'});
-        adapter.log.info('cleaned everything up...');
+        adapter.log.info('threema gateway stopped - cleaned everything up...');
         callback();
     } catch (e) {
         callback();
     }
 });
 
-/*// is called if a subscribed object changes
-adapter.on('objectChange', function (id, obj) {
-    // Warning, obj can be null if it was deleted
-    adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
-});
-
-// is called if a subscribed state changes
-adapter.on('stateChange', function (id, state) {
-    // Warning, state can be null if it was deleted
-    adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
-
-    // you can use the ack flag to detect if it is status (true) or command (false)
-    if (state && !state.ack) {
-        //adapter.log.info('ack is not set!');
-    }
-});*/
-
-// Some message was sent to adapter instance over message box. Used by email, pushover, text2speech, ...
+// Some message was sent to adapter instance over message box.
 adapter.on('message', function (obj) {
     if (typeof obj === 'object' && obj.message) {
         if (obj.command === 'send') {
-            // e.g. send email or pushover or whatever
+            // sendto command received
             adapter.log.info('send via Threema gateway: ' + obj.message);
             SendThreemaSimpleMessage(obj)
         }
@@ -52,10 +29,8 @@ adapter.on('message', function (obj) {
 
 var ThreemaRequest = require('request');
 var ThreemaSecret, ThreemaFrom, ThreemaTo;
-
 const ThreemaURL='https://msgapi.threema.ch';
 
-// is called when databases are connected and adapter received configuration.
 // start here!
 adapter.on('ready', function () {
     GetThreemaGWCredits();
@@ -63,58 +38,6 @@ adapter.on('ready', function () {
 });
 
 function main() {
-
-    // The adapters config (in the instance object everything under the attribute "native") is accessible via
-    // adapter.config:
-
-    // adapter.log.info('config API Secret: '    + ThreemaSecret);
-    // adapter.log.info('config Threema send from: '    + ThreemaFrom);
-    // adapter.log.info('config Threema send to: ' + ThreemaTo);
-
-
-    /**
-     *
-     *      For every state in the system there has to be also an object of type state
-     *
-     *      Here a simple template for a boolean variable named "testVariable"
-     *
-     *      Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-     *
-     */
-
-    /* adapter.setObject('SendMessage', {
-         type: 'state',
-         common: {
-             name: 'testVariable',
-             type: 'string',
-             role: 'text'
-         },
-         native: {}
-     });*/
-
-    // in this template all states changes inside the adapters namespace are subscribed
-    // adapter.subscribeStates('*');
-
-
-    /**
-     *   setState examples
-     *
-     *   you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-     *
-     */
-
-    // the variable testVariable is set to true as command (ack=false)
-    //adapter.setState('testVariable', true);
-
-    // same thing, but the value is flagged "ack"
-    // ack should be always set to true if the value is received from or acknowledged from the target system
-    // adapter.setState('testVariable', {val: true, ack: true});
-
-    // adapter.setState('info.connection', {val: true});
-    // adapter.setState('info.credits', {val: -999});
-    // same thing, but the state is deleted after 30s (getState will return null afterwards)
-    //adapter.setState('testVariable', {val: true, ack: true, expire: 30});
-
 
 }
 
@@ -190,7 +113,7 @@ function SendThreemaSimpleMessage(obj){
         ThreemaRequest.post({url:ThreemaGatewaySendSimple, form: formdata}, function (error, response, result) {
             switch(response.statusCode){
                 case 200:
-                    responseFromAPI = '200 - connection successful';
+                    responseFromAPI = '200 - message send successful';
                     //adapter.setState('info.credits', {val: result});    //amount of credits at Threema gateway
                     adapter.setState('info.connection', {val: true});  //connection to Threema gateway established
                     break;
